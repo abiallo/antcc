757d35cf05d9f5e08293b06bde703f4f3aacbd8d
Antonio <abiallo@gmail.com>
2012-05-10 22:27:38 +0400

    added hello

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, :notice => "Logged in successfully"
+      @session_name = user.name
+      redirect_to root_path, :notice => "Hello " + @session_name + ", logged in successfully"
     else
       flash.now[:alert] = "Invaild login/password combination"
       render :action => 'new'
@@ -12,6 +13,6 @@ class SessionsController < ApplicationController
   
   def destroy
     reset_session
-    redirect_to root_path, :notice => "You successfully logged out"
+    redirect_to root_path, :notice => "successfully logged out"
   end
 end
