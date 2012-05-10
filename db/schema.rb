dccde1140240ddc2da69a4c60217b98fef979dff
Antonio <abiallo@gmail.com>
2012-05-10 07:30:53 +0400

    added name to users

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111027031437) do
+ActiveRecord::Schema.define(:version => 20120510031724) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -96,6 +97,7 @@ ActiveRecord::Schema.define(:version => 20111027031437) do
     t.string   "hashed_password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
